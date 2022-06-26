6441df3b457d2364b044c6fcedda22302a74a5ba
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-06-26 21:07:36 +0900

    add Background Blur option

--- ccapreferences/Sources/CCAPreferences/RootListController.swift
+++ ccapreferences/Sources/CCAPreferences/RootListController.swift
@@ -33,7 +33,10 @@ class RootListController: PSListController {
     
     override func readPreferenceValue(_ specifier: PSSpecifier!) -> Any! {
         let prefs = NSDictionary(contentsOfFile: Self.PREF_PATH)
-        return prefs?.object(forKey: specifier.properties.object(forKey: "key") as Any)
+        let key = specifier.properties.object(forKey: "key") as Any
+        let `default` = specifier.properties.object(forKey: "default") as Any
+        
+        return prefs?.object(forKey: key) ?? prefs?.object(forKey: `default`)
     }
     
     override func setPreferenceValue(_ value: Any!, specifier: PSSpecifier!) {
